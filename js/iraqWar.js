99372ce9d36cbe94334942f628eafb9a6430d13f
lukellyk <luke-k@live.com>
2021-05-04 10:17:13 +1000

    Added map

---
+++ js/iraqWar.js
@@ -0,0 +1,102 @@
+const dataSourceI = "../data/iraq-war-data-indexed.csv"
+
+function loadDataI() {
+    Plotly.d3.csv(dataSourceI, function (data) {
+        console.log(data);
+      processDataI(data);
+    });
+  }
+
+function processDataI(allRows) {
+    let x = [], cmr = [], hdi = [], pcmr = [], phdi = [];
+    for (let i = 0; i < allRows.length; i++) {
+        let row = allRows[i];
+        x.push(row['Year']);
+        cmr.push(row['CMR']);
+        hdi.push(row['HDI']);
+        pcmr.push(row['pCMR']);
+        phdi.push(row['pHDI']);
+    }
+    makePlotI(x,cmr,hdi,pcmr,phdi);
+}   
+
+function makePlotI(x,cmr,hdi,pcmr,phdi){
+    var traces = [
+    {
+        name: 'Child Mortality Rate',
+        x: x,
+        y: cmr,
+        connectgaps: true
+    },
+    {
+        name: 'Human Development Index',
+        x: x,
+        y: hdi
+    },
+    {
+        name: 'Predicted Child Mortality Rate',
+        x: x,
+        y: pcmr,
+        line:{
+            dash: 'dot',
+            color: 'rgb(255,0,0)'
+        }
+    },
+    {
+        name: 'Predicted Human Development Index',
+        x: x,
+        y: phdi,
+        line:{
+            dash: 'dot',
+            color: 'rgb(255,0,0)'
+        }
+    }];
+
+    var layout = {
+        font: {
+            size: 18,
+            family: "Bilo",
+            color: 'c4c4c4'
+        },
+        xaxis: {
+            title: "Year"
+        },
+        yaxis: {
+            title: "Rate of Change (Indexed to 100)",
+        },
+        showlegend: false,
+        legend: {
+            x: 0.1,
+            xanchor: "left",
+            y: 0.9
+        },
+        margin: {
+            t: 20,
+            b: 50,
+            r: 50,
+        },
+        annotations: [
+            {
+              x: 2003,
+              y: 0,
+              xref: 'x',
+              yref: 'y',
+              text: 'Beginning of War',
+              showarrow: true,
+              arrowhead: 0,
+              ax: 0,
+              ay: -350
+            }
+          ]
+    };
+
+    var config = {
+        responsive: true,
+        scrollZoom: true,
+        displayModeBar: false,
+    };
+    console.log('iraq')
+    Plotly.newPlot('iraqWarPlot', traces, layout, config);
+};
+
+loadDataI();
---
+++ js/warMapPlot.js
@@ -0,0 +1,46 @@
+let countries = ['Iraq', 'Afghanistan', 'Kuwait']
+let dumbyData = [1,1,1]
+
+var data = [{
+    type: 'choropleth',
+    locationmode: 'country names',
+    locations: countries,
+    z: dumbyData,
+    colorscale: [
+        [0, 'rgb(255,255,255)'], [1, 'rgb(100,149,237)']
+    ],
+    showscale: false,
+    zmin: 0,
+    zmax: 1,
+    marker: {
+        line:{
+            width: 0
+        }
+    },
+    hovermode: 'closest',
+}];
+
+var layout = {
+    geo: {
+        projection: {
+            type: 'robinson',
+        },
+        showframe: false
+    },    
+    font: {
+        size: 18,
+        family: "Bilo",
+        color: 'c4c4c4'
+    },
+    showlegend: false,
+    margin: {
+        t: 0,
+        b: 0,
+        r: 10,
+        l: 10
+    },
+    paper_bgcolor: 'rgba(0,0,0,0)',
+    plot_bgcolor: 'rgba(0,0,0,0)'
+};
+
+Plotly.newPlot("warMapPlot", data, layout);
